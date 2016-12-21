874830f39da3717ada06f18a8ac50e1cca57fee6
Joshua Glover <joshua.glover@klarna.com>
2016-12-21 14:38:35 +0100

    Start with no code at all

--- src/cljs/quilt/db.cljs
+++ src/cljs/quilt/db.cljs
@@ -1,16 +1,7 @@
 (ns quilt.db)
 
 (def default-db
-  {:code [{:fun :clear
-           :index 0}
-          {:fun :color
-           :index 1
-           :color [0 0 0]}
-          {:fun :text
-           :index 2
-           :position [320 240]
-           :size 24
-           :text "Hello, world!"}]
+  {:code []
    :sketch {:name "sketch"
             :size [640 480]
             :bg-color [235 235 224]}})
