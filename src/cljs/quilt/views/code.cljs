fd01c689324cdb7b927615b37cff9bb21b126d0d
Joshua Glover <joshua.glover@klarna.com>
2016-12-21 16:50:48 +0100

    Render text form

--- src/cljs/quilt/views/code.cljs
+++ src/cljs/quilt/views/code.cljs
@@ -6,27 +6,40 @@
 (defn- replace-code [form]
   (rf/dispatch [:replace-code form]))
 
-(defn- input-assoc [size form path]
-  (let [get-value (fnil #(-> % .-target .-value read-string) 0)]
+(defn- input-assoc [size form path tx default]
+  (let [get-value #(-> % .-target .-value tx (or default))]
     [:input {:type "text"
-             :size (str size)
-             :maxLength (str size)
-             :value (str (or (get-in form path) 0))
-             :on-change #(let [new-form (assoc-in form path (get-value %))]
-                           (println path "changed:" (get-value %))
-                           (println "New val:" form)
-                           (replace-code new-form))}]))
+             :size size
+             :maxLength size
+             :value (str (get-in form path))
+             :on-change #(let [new-value (get-value %)]
+                           (replace-code (assoc-in form path new-value)))}]))
+
+(defn- input-num [size form path]
+  (input-assoc size form path read-string 0))
+
+(defn- input-text [size form path]
+  (input-assoc size form path identity ""))
 
 (defn- render-color [form]
-  (println "Rendering color form:" form)
   [:div
    "(color "
-   (input-assoc 3 form [:color 0]) " "
-   (input-assoc 3 form [:color 1]) " "
-   (input-assoc 3 form [:color 2]) ")"])
+   "[" (input-num 3 form [:color 0]) " "
+   (input-num 3 form [:color 1]) " "
+   (input-num 3 form [:color 2]) "])"])
+
+(defn- render-text [form]
+  (println "Rendering text form:" form)
+  [:div
+   "(text "
+   (input-text 64 form [:text]) " "
+   "[" (input-num 3 form [:position 0]) " "
+   (input-num 3 form [:position 1]) "] "
+   (input-num 2 form [:size]) ")"])
 
 (defn render [{:keys [fun] :as form}]
   (case fun
     :clear [:div "(clear)"]
     :color (render-color form)
+    :text (render-text form)
     [:div (pr-str form)]))
