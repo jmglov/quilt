012404305539a2cf7b37d9cc5a9543ca82a455b2
Joshua Glover <joshua.glover@klarna.com>
2016-12-31 10:04:57 +0100

    Drag and drop reordering of forms (WIP)

--- src/cljs/quilt/util.cljs
+++ src/cljs/quilt/util.cljs
@@ -8,3 +8,25 @@
    (-> element .-target .-value))
   ([element tx default]
    (-> element get-value tx (or default))))
+
+(defn client-rect
+  "Returns the position of the parent element as a vector of [x y], or
+    [nil nil] if there is no element or parent element"
+  [elem]
+  (when elem
+    (when-let [r (.getBoundingClientRect elem)]
+      [(.-left r) (.-top r)])))
+
+(defn relative-position
+  "Returns the position of the element relative to its parent element as a
+   vector of [x y]"
+  [elem]
+  (when elem
+    (when-let [[left top] (client-rect elem)]
+      [(- (.-clientX elem) left)
+       (- (.-clientY elem) top)])))
+
+(defn mouse-position
+  "Returns the position of the mouse from the event as a vector of [x y]"
+  [event]
+  [(.-clientX event) (.-clientY event)])
--- src/cljs/quilt/views/code.cljs
+++ src/cljs/quilt/views/code.cljs
@@ -1,11 +1,47 @@
 (ns quilt.views.code
   (:require [cljs.reader :refer [read-string]]
+            [goog.events :as events]
             [quilt.code :as code]
             [quilt.color :as color]
-            [quilt.util :refer [concatv get-value]]
+            [quilt.util :as util]
             [quilt.views.widgets :as widgets]
             [re-frame.core :as rf]
-            [reagent.core :as r]))
+            [reagent.core :as r])
+  (:import (goog.events EventType)))
+
+;; Make HTMLCollection a Clojure collection
+(extend-type js/HTMLCollection
+  ICounted
+  (-count [coll] (. coll -length))
+
+  IIndexed
+  (-nth
+    ([coll n] (. coll (item n)))
+    ([coll n not-found] (if (<= (. coll -length) n)
+                          not-found
+                          (nth coll n))))
+
+  ISeqable
+  (-seq [coll] (array-seq coll 0)))
+
+(def dragging-form (atom nil))
+(def dragging-listener-installed? (atom false))
+
+(defn on-mouse-up [event]
+  (when-let [{:keys [form elem siblings]} @dragging-form]
+    (println "Dropping at" (pr-str (util/mouse-position event)))
+    (reset! dragging-form nil)))
+
+(defn- mouse-down-handler [form]
+  (fn [event]
+    (let [elem (.-target event)]
+      (reset! dragging-form {:form form
+                             :elem elem
+                             :siblings (-> elem .-parentNode .-children)}))
+    (println "Dragging from" (pr-str (util/mouse-position event)))
+    (when-not @dragging-listener-installed?
+      (events/listen js/window EventType.MOUSEUP on-mouse-up)
+      (reset! dragging-listener-installed? true))))
 
 (defn- delete-code [form]
   (rf/dispatch [:delete-code form]))
@@ -24,19 +60,19 @@
   (input-assoc size form path identity ""))
 
 (defn- color-picker [form]
-  (concatv
+  (util/concatv
    [:select
     {:value (str (:color form))
-     :on-change #(let [new-color (read-string (get-value %))]
+     :on-change #(let [new-color (read-string (util/get-value %))]
                    (replace-code (assoc form :color new-color)))}]
    (map (fn [c] [:option (str c)])
         color/basic)))
 
 (defn- orientation-picker [form]
-  (concatv
+  (util/concatv
    [:select
     {:value (str (:orientation form))
-     :on-change #(let [new-value (read-string (get-value %))]
+     :on-change #(let [new-value (read-string (util/get-value %))]
                    (replace-code (assoc form :orientation new-value)))}]
    (map (fn [c] [:option (str c)])
         code/orientations)))
@@ -94,12 +130,12 @@
    (color-picker form) ")"])
 
 (defn render [{:keys [fun] :as form}]
-  (concatv [:div.form]
-           (case fun
-             :circle (render-circle form)
-             :curve (render-curve form)
-             :line (render-line form)
-             :rectangle (render-rectangle form)
-             :text (render-text form)
-             :triangle (render-triangle form))
-           [[:button {:on-click #(delete-code form)} "Delete"]]))
+  (util/concatv [:div.form {:on-mouse-down (mouse-down-handler form)}]
+                (case fun
+                  :circle (render-circle form)
+                  :curve (render-curve form)
+                  :line (render-line form)
+                  :rectangle (render-rectangle form)
+                  :text (render-text form)
+                  :triangle (render-triangle form))
+                [[:button {:on-click #(delete-code form)} "Delete"]]))
