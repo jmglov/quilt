db6df2377287648a46fb517184d4ee8266efb482
Joshua Glover <joshua.glover@klarna.com>
2017-01-03 13:40:13 +0100

    Use Google Closure libs to simplify reordering

--- src/cljs/quilt/util.cljs
+++ src/cljs/quilt/util.cljs
@@ -1,7 +1,18 @@
-(ns quilt.util)
+(ns quilt.util
+  (:require [goog.array :as garray]
+            [goog.dom :as gdom]
+            [goog.style :as gstyle]))
+
+
+;; Hiccup
+;; -----------------------------------------------------------------------------
 
 (defn concatv [& vs]
   (into [] (apply concat vs)))
+
+
+;; Styling
+;; -----------------------------------------------------------------------------
 
 (defn add-class [elem css-class]
   (-> elem .-classList (.add css-class)))
@@ -9,18 +20,9 @@
 (defn remove-class [elem css-class]
   (-> elem .-classList (.remove css-class)))
 
-(defn ancestor
-  "Returns the nth ancestor of elem"
-  [elem n]
-  (when (and elem (pos-int? n))
-    (loop [n n
-           elem elem]
-      (if (or (= 0 n) (nil? elem))
-        elem
-        (recur (dec n) (.-parentElement elem))))))
 
-(defn parent [elem]
-  (ancestor elem 1))
+;; Forms
+;; -----------------------------------------------------------------------------
 
 (defn get-value
   ([element]
@@ -28,35 +30,24 @@
   ([element tx default]
    (-> element get-value tx (or default))))
 
-(defn position
-  "Returns the position of the element as a vector of [x y], or
-   [nil nil] if there is no element"
-  [elem]
-  (when elem
-    (when-let [r (.getBoundingClientRect elem)]
-      [(.-left r) (.-top r)])))
 
-(defn parent-position
-  "Returns the position of the element's parent as a vector of [x y], or
-   [nil nil] if there is no element or the element has no parent (i.e. is
-   the top-level element in the DOM)"
-  [elem]
-  (and elem (position (.-parentElement elem))))
+;; DOM
+;; -----------------------------------------------------------------------------
+
+(defn children [elem]
+  (-> (gdom/getChildren elem)
+      garray/toArray))
+
+(defn get-element [id]
+  (gdom/getRequiredElement id))
+
+(defn parent [elem]
+  (gdom/getParentElement elem))
 
 (defn relative-position
-  "Returns the position of an element relative to its parent as a vector of
-   [x y], or [nil nil] if either the element or the parent are nil. If two
-   elements are specified, returns position of elem1 relative to elem2.
-   Elements may be DOM elements or position vectors."
-  ([elem]
-   (relative-position elem (parent-position elem)))
-  ([elem1 elem2]
-   (when (and elem1 elem2)
-     (let [[x1 y1] (if (vector? elem1) elem1 (position elem1))
-           [x2 y2] (if (vector? elem2) elem2 (position elem2))]
-       [(- x1 x2) (- y1 y2)]))))
-
-(defn mouse-position
-  "Returns the position of the mouse from the event as a vector of [x y]"
-  [event]
-  [(.-clientX event) (.-clientY event)])
+  "Returns the position of one element or mouse event relative to another as a
+   vector of [x y], or [nil nil] if either of the elements / events are nil"
+  ([e1 e2]
+   (when (and e1 e2)
+     (let [pos (gstyle/getRelativePosition e1 e2)]
+       [(.-x pos) (.-y pos)]))))
--- src/cljs/quilt/views/code/reorder.cljs
+++ src/cljs/quilt/views/code/reorder.cljs
@@ -4,58 +4,42 @@
             [re-frame.core :as rf])
   (:import (goog.events EventType)))
 
-;; Make HTMLCollection a Clojure collection
-(extend-type js/HTMLCollection
-  ICounted
-  (-count [coll] (. coll -length))
-
-  IIndexed
-  (-nth
-    ([coll n] (. coll (item n)))
-    ([coll n not-found] (if (<= (. coll -length) n)
-                          not-found
-                          (nth coll n))))
-
-  ISeqable
-  (-seq [coll] (array-seq coll 0)))
-
 (def ^:private selected-form (atom nil))
 (def handler-installed? (atom false))
 
-(defn- forms-elem [handle]
-  (util/ancestor handle 2))
+(defn- forms-element []
+  (util/get-element "forms"))
 
-(defn- form-position [handle]
-  (util/relative-position handle (forms-elem handle)))
+(defn- form-elements []
+  (util/children (forms-element)))
 
-(defn- siblings [form-elem]
-  (.-children (util/parent form-elem)))
+(defn- form-position [form-elem]
+  (util/relative-position form-elem (forms-element)))
+
+(defn- form-for [handle-elem]
+  (util/parent handle-elem))
+
+(defn- select-form [form-elem]
+  (util/add-class form-elem "selected-form"))
+
+(defn- unselect-form [form-elem]
+  (util/remove-class form-elem "selected-form"))
 
 (defn index-before [selected-elem y index form-elems i]
   (let [cur-elem (nth form-elems i)
-        [_ cur-y] (util/relative-position cur-elem)
+        [_ cur-y] (form-position cur-elem)
         cur-y (quot (.-clientHeight cur-elem) 2)]
     (println "Form at index" i "y position:" cur-y)
     (when (and (not (identical? selected-elem cur-elem))
                (<= y cur-y))
       (if (<= i index) i (dec i)))))
 
-(defn- form-for [handle]
-  (util/parent handle))
-
-(defn- select-form [form]
-  (util/add-class form "selected-form"))
-
-(defn- unselect-form [form]
-  (util/remove-class form "selected-form"))
-
 (defn- on-mouse-up [event]
   (when-let [{:keys [form selected-elem]} @selected-form]
     (let [{:keys [fun index]} form
-          [_ mouse-y] (util/relative-position (util/mouse-position event)
-                                              (util/parent selected-elem))
+          [_ mouse-y] (util/relative-position event (forms-element))
           _ (println "Dropping at" mouse-y)
-          form-elems (siblings selected-elem)
+          form-elems (form-elements)
           _ (println "Form elems" form-elems)
           before-form (partial index-before
                                selected-elem mouse-y index form-elems)
@@ -70,10 +54,11 @@
 (defn- mouse-down-handler [{:keys [fun index] :as form}]
   (fn [event]
     (let [handle (.-target event)
-          [_ y] (form-position handle)]
-      (select-form (form-for handle))
-      (reset! selected-form {:form form, :selected-elem (util/parent handle)})
-      (println (str "Dragging " fun " (index " index ") from y position " y)))
+          form-elem (form-for handle)
+          [_ cur-y] (util/relative-position form-elem (forms-element))]
+      (select-form form-elem)
+      (reset! selected-form {:form form, :selected-elem form-elem})
+      (println (str "Dragging " fun " (index " index ") from y position " cur-y)))
     (when-not @handler-installed?
       (events/listen js/window EventType.MOUSEUP on-mouse-up)
       (reset! handler-installed? true))))
