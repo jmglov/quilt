c7ae6163752f84adb2605ee8eb3cfa6f148c0dd6
Joshua Glover <joshua.glover@klarna.com>
2017-01-26 08:44:45 +0100

    Remove lo-res mode

--- src/cljs/quilt/views/code.cljs
+++ src/cljs/quilt/views/code.cljs
@@ -3,7 +3,6 @@
             [clojure.string :as string]
             [quilt.code :as code]
             [quilt.color :as color]
-            [quilt.sketch.resolution :as resolution]
             [quilt.util :as util :refer [concatv]]
             [quilt.views.code.reorder :as reorder]
             [quilt.views.widgets :as widgets]
@@ -23,17 +22,17 @@
     (rf/dispatch [:show-docstring [index param]])))
 
 (defn- input-assoc
-  [size {:keys [index] :as form} [param & _ :as path] tx default scale display]
-  (-> (widgets/input #(replace-code (assoc-in form path (scale %)))
-                     size (str (display (get-in form path))) tx default)
+  [size {:keys [index] :as form} [param & _ :as path] tx default]
+  (-> (widgets/input #(replace-code (assoc-in form path %))
+                     size (str (get-in form path)) tx default)
       (assoc-in [1 :onFocus] (show-param-docstring index param))))
 
 (defn- input-num
-  [size {:keys [index] :as form} [param & _ :as path] scale display]
-  (input-assoc size form path read-string 0 scale display))
+  [size {:keys [index] :as form} [param & _ :as path]]
+  (input-assoc size form path read-string 0))
 
 (defn- input-text [size form path]
-  (input-assoc size form path identity "" identity identity))
+  (input-assoc size form path identity ""))
 
 (defn- color-picker [form]
   (let [colors (-> color/basic
@@ -58,87 +57,84 @@
    (map (fn [c] [:option (str c)])
         code/orientations)))
 
-(defn- render-readonly [form args display]
-  (let [pr-val (fn [v] (if (string? v) v (display v)))]
-    [(str (->> (concat [:position] args [:color])
-               (map #(pr-val (form %)))
-               (string/join " ")))]))
+(defn- render-readonly [form args]
+  [(str (->> (concat [:position] args [:color])
+             (map #(form %))
+             (string/join " ")))])
 
-(defn- render-circle [form readonly? scale display]
+(defn- render-circle [form readonly?]
   (if readonly?
-    (render-readonly form [:radius] display)
+    (render-readonly form [:radius])
     ["["
-     (input-num 3 form [:position 0] scale display) " "
-     (input-num 3 form [:position 1] scale display) "] "
-     (input-num 3 form [:radius] scale display) " "
+     (input-num 3 form [:position 0]) " "
+     (input-num 3 form [:position 1]) "] "
+     (input-num 3 form [:radius]) " "
      (color-picker form)]))
 
-(defn- render-curve [form readonly? scale display]
+(defn- render-curve [form readonly?]
   (if readonly?
-    (render-readonly form [:radius] display)
+    (render-readonly form [:radius])
     ["["
-     (input-num 3 form [:position 0] scale display) " "
-     (input-num 3 form [:position 1] scale display) "] "
-     (input-num 3 form [:radius] scale display) " "
+     (input-num 3 form [:position 0]) " "
+     (input-num 3 form [:position 1]) "] "
+     (input-num 3 form [:radius]) " "
      (orientation-picker form) " "
-     (input-num 2 form [:thickness] scale display) " "
+     (input-num 2 form [:thickness]) " "
      (color-picker form)]))
 
-(defn- render-dot [form readonly? scale display]
+(defn- render-dot [form readonly?]
   (if readonly?
-    (render-readonly form [] display)
+    (render-readonly form [])
     ["["
-     (input-num 3 form [:position 0] scale display) " "
-     (input-num 3 form [:position 1] scale display) "] "
+     (input-num 3 form [:position 0]) " "
+     (input-num 3 form [:position 1]) "] "
      (color-picker form)]))
 
-(defn- render-line [form readonly? scale display]
+(defn- render-line [form readonly?]
   (if readonly?
-    (render-readonly form [:thickness] display)
+    (render-readonly form [:thickness])
     ["[["
-     (input-num 3 form [:position 0 0] scale display) " "
-     (input-num 3 form [:position 0 1] scale display) "] ["
-     (input-num 3 form [:position 1 0] scale display) " "
-     (input-num 3 form [:position 1 1] scale display) "]] "
-     (input-num 2 form [:thickness] scale display) " "
+     (input-num 3 form [:position 0 0]) " "
+     (input-num 3 form [:position 0 1]) "] ["
+     (input-num 3 form [:position 1 0]) " "
+     (input-num 3 form [:position 1 1]) "]] "
+     (input-num 2 form [:thickness]) " "
      (color-picker form)]))
 
-(defn- render-rectangle [form readonly? scale display]
+(defn- render-rectangle [form readonly?]
   (if readonly?
-    (render-readonly form [:width :height] display)
+    (render-readonly form [:width :height])
     ["["
-     (input-num 3 form [:position 0] scale display) " "
-     (input-num 3 form [:position 1] scale display) "] "
-     (input-num 3 form [:width] scale display) " "
-     (input-num 3 form [:height] scale display) " "
+     (input-num 3 form [:position 0]) " "
+     (input-num 3 form [:position 1]) "] "
+     (input-num 3 form [:width]) " "
+     (input-num 3 form [:height]) " "
      (color-picker form)]))
 
-(defn- render-text [form readonly? scale display]
+(defn- render-text [form readonly?]
   (if readonly?
-    (render-readonly form [:text :size] display)
+    (render-readonly form [:text :size])
     ["["
-     (input-num 3 form [:position 0] scale display) " "
-     (input-num 3 form [:position 1] scale display) "] "
+     (input-num 3 form [:position 0]) " "
+     (input-num 3 form [:position 1]) "] "
      (input-text 30 form [:text]) " "
-     (input-num 2 form [:size] scale display) " "
+     (input-num 2 form [:size]) " "
      (color-picker form)]))
 
-(defn- render-triangle [form readonly? scale display]
+(defn- render-triangle [form readonly?]
   (if readonly?
-    (render-readonly form [] display)
+    (render-readonly form [])
     ["[["
-     (input-num 3 form [:position 0 0] scale display) " "
-     (input-num 3 form [:position 0 1] scale display) "] ["
-     (input-num 3 form [:position 1 0] scale display) " "
-     (input-num 3 form [:position 1 1] scale display) "] ["
-     (input-num 3 form [:position 2 0] scale display) " "
-     (input-num 3 form [:position 2 1] scale display) "]] "
+     (input-num 3 form [:position 0 0]) " "
+     (input-num 3 form [:position 0 1]) "] ["
+     (input-num 3 form [:position 1 0]) " "
+     (input-num 3 form [:position 1 1]) "] ["
+     (input-num 3 form [:position 2 0]) " "
+     (input-num 3 form [:position 2 1]) "]] "
      (color-picker form)]))
 
 (defn render [{:keys [fun index] :as form} editor-atom sketch]
-  (let [scale #(resolution/scale sketch %)
-        display #(resolution/display sketch %)
-        {:keys [readonly? selected-index]} @editor-atom
+  (let [{:keys [readonly? selected-index]} @editor-atom
         css-classes (str "form unselectable container"
                          (when (= selected-index index) " selected-form"))]
     (concatv
@@ -152,13 +148,13 @@
        [(concatv
          [:div (str "(" (name fun) " ")]
          (case fun
-           :circle (render-circle form readonly? scale display)
-           :curve (render-curve form readonly? scale display)
-           :dot (render-dot form readonly? scale display)
-           :line (render-line form readonly? scale display)
-           :rectangle (render-rectangle form readonly? scale display)
-           :text (render-text form readonly? scale display)
-           :triangle (render-triangle form readonly? scale display))
+           :circle (render-circle form readonly?)
+           :curve (render-curve form readonly?)
+           :dot (render-dot form readonly?)
+           :line (render-line form readonly?)
+           :rectangle (render-rectangle form readonly?)
+           :text (render-text form readonly?)
+           :triangle (render-triangle form readonly?))
          [")"])]
        [[:div.delete-form {:on-click #(delete-code form)} "✖"]])]
      (when-let [[displayed-index param] (:displayed-docstring @editor-atom)]
